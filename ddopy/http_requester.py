68f586cb20498de31ef38a4f7d51e84f4899fae1
Ali Burak Kulakli <kulakli@gmail.com>
2023-08-21 17:48:48 +0300

    Add header support

--- ddopy/http_requester.py
+++ ddopy/http_requester.py
@@ -5,16 +5,19 @@ import requests
 class HttpRequester:
     def __init__(self):
         self._endpoint_url = None
+        self._headers = {}
 
     def set_endpoint_url(self, url):
         self._endpoint_url = url
+
+    def add_header(self, key, value):
+        self._headers[key] = value
 
     def post_request(self, payload):
         if self._endpoint_url is None:
             raise Exception("Endpoint URL is not set. Call set_endpoint_url() method first.")
 
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(url=self._endpoint_url, data=json.dumps(payload), headers=headers, timeout=5)
+        response = requests.post(url=self._endpoint_url, data=json.dumps(payload), headers=self._headers, timeout=5)
         response_json = response.json()
 
         return response_json
