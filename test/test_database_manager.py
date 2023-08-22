d3afd66119fcbc9c0584fca0d235174b3689e67d
Ali Burak Kulakli <kulakli@gmail.com>
2023-08-22 09:44:58 +0300

    added unittests

---
+++ test/test_database_manager.py
@@ -0,0 +1,9 @@
+# write unit tests for database_manager.py
+
+import unittest
+
+
+class TestDatabaseManager(unittest.TestCase):
+
+    def test_1(self):
+        self.assertTrue(True)
---
+++ test/test_http_requester.py
@@ -0,0 +1,12 @@
+# write unit tests for database_manager.py
+
+import unittest
+
+
+class TestHttpRequester(unittest.TestCase):
+
+    def test_pass(self):
+        self.assertTrue(True)
+
+    def test_fail(self):
+        self.assertTrue(False)
