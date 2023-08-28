0434fb4da3fcaa298125ccac4d86ba938151ee44
Ali Burak Kulakli <kulakli@gmail.com>
2023-08-28 18:41:01 +0300

    http get request

--- ddopy/http/http_requester.py
+++ ddopy/http/http_requester.py
@@ -20,8 +20,16 @@ class HttpRequester:
         if self._base_url is None:
             raise Exception("Base URL is not set. Call set_base_url() method first.")
 
-
         url = f"{self._base_url}/{relative_url.lstrip('/')}".rstrip("/")
         response = requests.post(url=url, data=json.dumps(payload), headers=self._headers, timeout=30)
+
+        return response
+
+    def get_request(self, relative_url=""):
+        if self._base_url is None:
+            raise Exception("Base URL is not set. Call set_base_url() method first.")
+
+        url = f"{self._base_url}/{relative_url.lstrip('/')}".rstrip("/")
+        response = requests.get(url=url, headers=self._headers, timeout=30)
 
         return response
