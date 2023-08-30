50af1911c5915359aef4412a4e5be551e0a5616d
Ali Burak Kulakli <kulakli@gmail.com>
2023-08-30 09:44:22 +0300

    async http requester

--- ddopy/http/http_requester.py
+++ ddopy/http/http_requester.py
@@ -1,14 +1,14 @@
 """
 This module contains HttpRequester class which is used to send HTTP requests.
 """
+import aiohttp
 import json
-import requests
-
 
 class HttpRequester:
     def __init__(self):
         self._base_url = None
         self._headers = {}
+        self._session = None
 
     def set_base_url(self, url):
         self._base_url = url
@@ -16,20 +16,36 @@ class HttpRequester:
     def add_header(self, key, value):
         self._headers[key] = value
 
-    def post_request(self, payload, relative_url=""):
+    async def _get_session(self):
+        if self._session is None:
+            self._session = aiohttp.ClientSession(headers=self._headers)
+        return self._session
+
+    async def close_session(self):
+        if self._session:
+            await self._session.close()
+            self._session = None
+
+    async def get_request(self, relative_url=""):
         if self._base_url is None:
             raise Exception("Base URL is not set. Call set_base_url() method first.")
 
         url = f"{self._base_url}/{relative_url.lstrip('/')}".rstrip("/")
-        response = requests.post(url=url, data=json.dumps(payload), headers=self._headers, timeout=30)
 
-        return response
+        session = await self._get_session()
 
-    def get_request(self, relative_url=""):
+        async with session.get(url) as response:
+            await response.read()
+            return response
+
+    async def post_request(self, payload, relative_url=""):
         if self._base_url is None:
             raise Exception("Base URL is not set. Call set_base_url() method first.")
 
         url = f"{self._base_url}/{relative_url.lstrip('/')}".rstrip("/")
-        response = requests.get(url=url, headers=self._headers, timeout=30)
 
-        return response
+        session = await self._get_session()
+
+        async with session.post(url, data=json.dumps(payload)) as response:
+            await response.read()
+            return response
