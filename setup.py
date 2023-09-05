9dfe2d9969fed6176c79d56e2bd2f8ad37d7f043
Ali Burak Kulakli <kulakli@gmail.com>
2023-09-05 12:50:12 +0300

    initial websocket proxy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ddopy',
-    version='0.1.18',
+    version='0.1.19',
     description='A collection of utility functions by Ali Burak Kulakli @ DDOSOFT',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
