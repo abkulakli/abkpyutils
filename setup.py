dcd461d7ba878226803b5ed4b95cad8ae78ea150
Ali Burak KULAKLI <kulakli@gmail.com>
2024-12-02 09:45:09 +0300

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ddopy',
-    version='0.1.19',
+    version='0.1.20',
     description='A collection of utility functions by Ali Burak Kulakli @ DDOSOFT',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
